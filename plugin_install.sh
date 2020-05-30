2b847e709e9148b950693d94543971f1958df958
Xintao <wxt1994@126.com>
2020-05-31 00:26:13 +0800

    update configs

---
+++ plugin_install.sh
@@ -0,0 +1,7 @@
+npm install gitbook-plugin-simple-page-toc
+npm install gitbook-plugin-anchor-navigation-ex
+npm install gitbook-plugin-copy-code-button
+npm install gitbook-plugin-tbfed-pagefooter
+npm install gitbook-plugin-search-plus
+npm install gitbook-plugin-emphasize
+npm install gitbook-plugin-katex
